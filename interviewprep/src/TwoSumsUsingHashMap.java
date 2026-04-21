fbece191038f820714d6fede7fdd5694ff9b10ca
Mohana Krishna Kavali <mohanakrishnakavali@Mohanas-MacBook-Air.local>
2026-04-21 18:08:58 -0400

    Two Sums using HashMap

---
+++ interviewprep/src/TwoSumsUsingHashMap.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumsUsingHashMap {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int current=nums[i];
+            int need=target-current;
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+            map.put(current, i);
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        int target=9;
+        int[] result=twoSum(nums,target);
+        System.out.println("["+result[0]+","+result[1]+"]");
+    }
+}
