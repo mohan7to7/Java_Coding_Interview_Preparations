0424241260528232f78802bf1453f7c120c21492
Mohana Krishna Kavali <mohanakrishnakavali@Mohanas-MacBook-Air.local>
2026-04-21 20:26:17 -0400

    Checking valid parentheses

---
+++ interviewprep/src/ValidParentheses.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+
+        for(char c: s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+                if ((c == '}' && top != '{') ||
+                        (c == ']' && top != '[') ||
+                        (c == ')' && top != '(' )) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+    public static void main(String[] args) {
+        ValidParentheses p = new ValidParentheses();
+        System.out.println(p.isValid("()[]{}"));
+        System.out.println(p.isValid("([)]"));
+        System.out.println(p.isValid("([)]"));
+    }
+}
