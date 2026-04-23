081634a5a85e8d96609910bdb50f200ea3325509
mohan7to7 <mohan7to7@gmail.com>
2026-04-22 20:57:47 -0400

    started the PlusOne ....Not completed

---
+++ interviewprep/src/PlusOne.java
@@ -0,0 +1,13 @@
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+        for(int i : digits){
+
+        }
+        return null;
+    }
+
+    static void main(String[] args) {
+
+    }
+}
