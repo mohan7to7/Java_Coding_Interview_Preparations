b5bbdc0f4101edaae600e0a3c9641b18162be419
mohan7to7 <mohan7to7@gmail.com>
2026-04-22 21:06:40 -0400

    Not completed, still pending

---
+++ interviewprep/src/BinarySearch.java
@@ -0,0 +1,11 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 9;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+        }
+    }
+}
