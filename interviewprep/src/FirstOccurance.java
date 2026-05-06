1cbfbf2d3e85d99ec9c35d15453229f89dbfa13b
mohan7to7 <mohan7to7@gmail.com>
2026-05-05 21:04:08 -0400

    updated the FirstOccurance

--- interviewprep/src/FirstOccurance.java
+++ interviewprep/src/FirstOccurance.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class FirstOccurance {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+    public static void main(String[] args) {
+        FirstOccurance f = new FirstOccurance();
+       System.out.println("Answer : "+ f.strStr("sadbutsad","needle"));
+        System.out.println("Answer : "+ f.strStr("sadbutsad","sad"));
+    }
 }
