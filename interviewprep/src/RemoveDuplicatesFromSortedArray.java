fdc8bee883353be276bb77761a7d836a745de16c
mohan7to7 <mohan7to7@gmail.com>
2026-05-05 21:04:48 -0400

    updated the RemoveDuplicatesFromSortedArray

--- interviewprep/src/RemoveDuplicatesFromSortedArray.java
+++ interviewprep/src/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+
+        if(nums==null||nums.length==0){
+            return 0;
+        }
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+public static void main(String[] args) {
+    int[] nums = {1,1,2};
+    System.out.println("Answer : "+ removeDuplicates(nums));
+}
 }
